3d6c3377c2de55fe07c3700d432d1e644d6a12fa
Sevak Sahakyan <sevak.sahakyan1992@gmail.com>
2020-09-01 20:42:58 +0400

    Added theme

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+const primary = "#0292F2";
+const secondary = "#5A5A5A";
+
+export default {
+  palette: {
+    primary: {
+      main: primary,
+    },
+    secondary: {
+      main: secondary,
+    },
+  },
+  typography: {
+    fontFamily: "Lato",
+    button: {
+      textTransform: "capitalize",
+      fontSize: "1.125rem",
+    },
+  },
+};
