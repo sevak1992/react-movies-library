11fd70a5c1c59fca448a7c1f0ec346f7ea31e716
Sevak Sahakyan <sevak.sahakyan1992@gmail.com>
2020-09-01 20:26:21 +0400

    Added material ui

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,29 +5,35 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import theme from "theme";
 
 import routes from "routes";
 
 function App() {
+  const muiTheme = createMuiTheme(theme);
+
   return (
-    <div className="page-wrap">
-      <Router>
-        <Switch>
-          {routes.map((route) => {
-            const Component = route.component;
-            return (
-              <Route
-                key={route.id}
-                path={route.path}
-                exact={!!route.exact}
-                component={Component}
-              />
-            );
-          })}
-          <Redirect from="*" to="/" />
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <div className="page-wrap">
+        <Router>
+          <Switch>
+            {routes.map((route) => {
+              const Component = route.component;
+              return (
+                <Route
+                  key={route.id}
+                  path={route.path}
+                  exact={!!route.exact}
+                  component={Component}
+                />
+              );
+            })}
+            <Redirect from="*" to="/" />
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
