0e4b987620ad6f76eaf9559ba025c5ea47c37434
Sevak Sahakyan <sevak.sahakyan1992@gmail.com>
2020-09-01 18:45:53 +0400

    Bug fix

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import { Provider } from 'react-redux'
 
 import routes from 'routes';
-import store from 'store';
 
 function App() {
   return (
     <div
         className="page-wrap"
     >
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            {routes.map((route) => {
-              const Component = route.component;
-              return (
-                <Route
-                  key={route.id}
-                  path={route.path}
-                  exact={!!route.exact}
-                  component={Component}
-                />
-              );
-            })}
-            <Redirect from="*" to="/" />
-          </Switch>
+      <Router>
+        <Switch>
+          {routes.map((route) => {
+            const Component = route.component;
+            return (
+              <Route
+                key={route.id}
+                path={route.path}
+                exact={!!route.exact}
+                component={Component}
+              />
+            );
+          })}
+          <Redirect from="*" to="/" />
+        </Switch>
         </Router>
-      </Provider>
     </div>
   );
 }
