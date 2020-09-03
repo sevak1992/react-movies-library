aefeb47996758b2bfc24003b9f1fc7a8fca599f3
Sevak Sahakyan <sevak.sahakyan1992@gmail.com>
2020-09-03 22:40:36 +0400

    Code cleanup

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -13,25 +13,19 @@ export const getConfigs = () => tmdbApi.get("configuration");
 
 export const getGenres = () => tmdbApi.get("/genre/movie/list");
 
-export const getMovie = (id) => {
-  return tmdbApi.get(`/movie/${id}`);
-};
+export const getMovie = (id) => tmdbApi.get(`/movie/${id}`);
 
 export const getPopularMovies = (page = 1) =>
   tmdbApi.get("/movie/popular", { params: { page } });
 
-export const getRecommendedMoviesByMovie = (id) => {
-  return tmdbApi.get(`/movie/${id}/recommendations`);
-};
+export const getRecommendedMoviesByMovie = (id) =>
+  tmdbApi.get(`/movie/${id}/recommendations`);
 
-export const getSimilarMoviesByMovie = (id) => {
-  return tmdbApi.get(`/movie/${id}/similar`);
-};
+export const getSimilarMoviesByMovie = (id) =>
+  tmdbApi.get(`/movie/${id}/similar`);
 
 export const getMoviesByTerm = (searchTerm) => {
   console.log(searchTerm);
 };
 
-export const getMovieCredits = (id) => {
-  return tmdbApi.get(`/movie/${id}/credits`);
-};
+export const getMovieCredits = (id) => tmdbApi.get(`/movie/${id}/credits`);
