496aa9bedd5afb720bddbacf35bbaf9c1a75db3a
Sevak Sahakyan <sevak.sahakyan1992@gmail.com>
2020-09-06 19:57:04 +0400

    Updates

---
+++ precache-manifest.c6532e0dad7217b500ab04d3ee5c9355.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee70df530bfa170e5d8a38fd9bd5bce5",
+    "url": "/react-movies-library/index.html"
+  },
+  {
+    "revision": "86d972e9cfec63a15cab",
+    "url": "/react-movies-library/static/css/2.a612c53c.chunk.css"
+  },
+  {
+    "revision": "7ca6a32826015a06cf4b",
+    "url": "/react-movies-library/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "86d972e9cfec63a15cab",
+    "url": "/react-movies-library/static/js/2.a95ac463.chunk.js"
+  },
+  {
+    "revision": "af7fc023766ba547389180ad5fa947d6",
+    "url": "/react-movies-library/static/js/2.a95ac463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca6a32826015a06cf4b",
+    "url": "/react-movies-library/static/js/main.532584ea.chunk.js"
+  },
+  {
+    "revision": "b799cc9a93d73e709e7d",
+    "url": "/react-movies-library/static/js/runtime-main.0185763e.js"
+  },
+  {
+    "revision": "7ebd0560cb404fb90710f8733d4185d8",
+    "url": "/react-movies-library/static/media/noImage.7ebd0560.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-movies-library/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-movies-library/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-movies-library/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-movies-library/static/media/slick.f97e3bbf.svg"
+  }
+]);
